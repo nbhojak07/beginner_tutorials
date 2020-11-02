d392dce8dab5f5fd25dd51c935a758a6b2b4a538
nbhojak07 <nidhi.bhojak97@gmail.com>
2020-11-02 15:29:51 -0500

    Added ROS Service and Clinet
    [beginner_tutorials/src/talker.cpp] Add ROS Service
    
    [beginner_tutorials/src/listener.cpp] Add ROS Client

--- beginner_tutorials/src/listener.cpp
+++ beginner_tutorials/src/listener.cpp
@@ -28,6 +28,7 @@
 // %Tag(FULLTEXT)%
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+#include "beginner_tutorials/AddTwoInts.h"
 
 /**
  * This tutorial demonstrates simple receipt of messages over the ROS system.
@@ -51,6 +52,11 @@ int main(int argc, char **argv) {
    */
   ros::init(argc, argv, "listener");
 
+  if (argc != 3){
+    ROS_INFO("Usage: add_two_ints_client X Y");
+    return 1;
+  }
+
   /**
    * NodeHandle is the main access point to communications with the ROS system.
    * The first NodeHandle constructed will fully initialize this node, and the last
@@ -58,34 +64,18 @@ int main(int argc, char **argv) {
    */
   ros::NodeHandle n;
 
-  /**
-   * The subscribe() call is how you tell ROS that you want to receive messages
-   * on a given topic.  This invokes a call to the ROS
-   * master node, which keeps a registry of who is publishing and who
-   * is subscribing.  Messages are passed to a callback function, here
-   * called chatterCallback.  subscribe() returns a Subscriber object that you
-   * must hold on to until you want to unsubscribe.  When all copies of the Subscriber
-   * object go out of scope, this callback will automatically be unsubscribed from
-   * this topic.
-   *
-   * The second parameter to the subscribe() function is the size of the message
-   * queue.  If messages are arriving faster than they are being processed, this
-   * is the number of messages that will be buffered up before beginning to throw
-   * away the oldest ones.
-   */
-// %Tag(SUBSCRIBER)%
-  ros::Subscriber sub = n.subscribe("chatter", 1000, chatterCallback);
-// %EndTag(SUBSCRIBER)%
+// ROS Service Client
 
-  /**
-   * ros::spin() will enter a loop, pumping callbacks.  With this version, all
-   * callbacks will be called from within this thread (the main one).  ros::spin()
-   * will exit when Ctrl-C is pressed, or the node is shutdown by the master.
-   */
-// %Tag(SPIN)%
-  ros::spin();
-// %EndTag(SPIN)%
-
-  return 0;
+ros::ServiceClient client = n.serviceClient<beginner_tutorials::AddTwoInts>("add_two_ints");
+beginner_tutorials::AddTwoInts srv;
+srv.request.a = atoll(argv[1]);
+srv.request.b = atoll(argv[2]);
+if ( client.call(srv)){
+  ROS_INFO("Summation: %1d", (long int)srv.response.sum);
 }
-// %EndTag(FULLTEXT)%
+else{
+  ROS_ERROR("failed call service add_two_ints");
+  return -1;
+}
+return 0;
+}
--- beginner_tutorials/src/talker.cpp
+++ beginner_tutorials/src/talker.cpp
@@ -32,8 +32,16 @@
 // %Tag(MSG_HEADER)%
 #include "std_msgs/String.h"
 // %EndTag(MSG_HEADER)%
+#include "beginner_tutorials/AddTwoInts.h"
 
-
+// Funtion to provide service to add two ints
+bool add(beginner_tutorials::AddTwoInts::Request &req,
+         beginner_tutorials::AddTwoInts::Response &res){
+        res.sum = req.a + req.b;
+        ROS_INFO("Requst: x = %1d, y= %1d", (long int)req.a, (long int)req.b);
+        ROS_INFO("Sending response: [%1d]", (long int)res.sum);
+        return true;
+         }
 
 /**
  * This tutorial demonstrates simple sending of messages over the ROS system.
@@ -80,57 +88,17 @@ int main(int argc, char **argv) {
    * buffer up before throwing some away.
    */
 // %Tag(PUBLISHER)%
-  ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
 // %EndTag(PUBLISHER)%
 
 // %Tag(LOOP_RATE)%
   ros::Rate loop_rate(10);
 // %EndTag(LOOP_RATE)%
 
-  /**
-   * A count of how many messages we have sent. This is used to create
-   * a unique string for each message.
-   */
-// %Tag(ROS_OK)%
-  int count = 0;
-  while (ros::ok()) {
-// %EndTag(ROS_OK)%
-    /**
-     * This is a message object. You stuff it with data, and then publish it.
-     */
-// %Tag(FILL_MESSAGE)%
-    std_msgs::String msg;
+// Call service
+ros::ServiceServer service = n.advertiseService("add_two_ints", add);
+ROS_INFO("Adding two ints");
+ros::spin();
 
-    std::stringstream ss;
-    ss << "Hello Terps !!!" << count;
-    msg.data = ss.str();
-// %EndTag(FILL_MESSAGE)%
-
-// %Tag(ROSCONSOLE)%
-    ROS_INFO("%s", msg.data.c_str());
-// %EndTag(ROSCONSOLE)%
-
-    /**
-     * The publish() function is how you send messages. The parameter
-     * is the message object. The type of this object must agree with the type
-     * given as a template parameter to the advertise<>() call, as was done
-     * in the constructor above.
-     */
-// %Tag(PUBLISH)%
-    chatter_pub.publish(msg);
-// %EndTag(PUBLISH)%
-
-// %Tag(SPINONCE)%
-    ros::spinOnce();
-// %EndTag(SPINONCE)%
-
-// %Tag(RATE_SLEEP)%
-    loop_rate.sleep();
-// %EndTag(RATE_SLEEP)%
-    ++count;
-  }
-
-
-  return 0;
+return 0;
 }
 // %EndTag(FULLTEXT)%
