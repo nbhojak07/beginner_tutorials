904cff737d087275cff0f08161ca6a1852abd569
nbhojak07 <nidhi.bhojak97@gmail.com>
2020-11-02 23:21:21 -0500

    Added Doxygen Style Commenting
    [beginner_tutorials/src/listener.cpp] Add Doxygen Comments
    
    [beginner_tutorials/src/talker.cpp] Add Doxygen Comments

--- beginner_tutorials/src/listener.cpp
+++ beginner_tutorials/src/listener.cpp
@@ -1,30 +1,17 @@
 /*
  * Copyright (C) 2008, Morgan Quigley and Willow Garage, Inc.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *   * Redistributions of source code must retain the above copyright notice,
- *     this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above copyright
- *     notice, this list of conditions and the following disclaimer in the
- *     documentation and/or other materials provided with the distribution.
- *   * Neither the names of Stanford University or Willow Garage, Inc. nor the names of its
- *     contributors may be used to endorse or promote products derived from
- *     this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- */
-
+ * Copyright 2020 Nidhi Bhojak 
+ * @file listener.cpp 
+ * @author Nidhi Bhojak
+ * @date 11/02/2020
+ * 
+ * @brief ROS Client 
+ * 
+ * @section DESCRIPTION 
+ * 
+ * Source file for ROS client to add two integers 
+ * */
+ 
 // %Tag(FULLTEXT)%
 #include "ros/ros.h"
 #include "std_msgs/String.h"
@@ -33,7 +20,12 @@
 /**
  * This tutorial demonstrates simple receipt of messages over the ROS system.
  */
-
+/**
+ * @brief Main function
+ * @param argc Command line number of arguments
+ * @param argv Command line arguments vector 
+ * @return 0
+ *  **/
 int main(int argc, char **argv) {
   /**
    * The ros::init() function needs to see argc and argv so that it can perform
@@ -45,8 +37,9 @@ int main(int argc, char **argv) {
    * You must call one of the versions of ros::init() before using any other
    * part of the ROS system.
    */
+  // Initialise ROS Node 
   ros::init(argc, argv, "listener");
-
+  // Check argument length vector 
   if (argc != 3){
     ROS_INFO_STREAM("Usage: add_two_ints_client X Y");
     return 1;
@@ -57,6 +50,7 @@ int main(int argc, char **argv) {
    * The first NodeHandle constructed will fully initialize this node, and the last
    * NodeHandle destructed will close down the node.
    */
+  // Create ROS Node handle object 
   ros::NodeHandle n;
 
 // ROS Service Client
@@ -65,6 +59,7 @@ ros::ServiceClient client = n.serviceClient<beginner_tutorials::AddTwoInts>("add
 beginner_tutorials::AddTwoInts srv;
 srv.request.a = atoll(argv[1]);
 srv.request.b = atoll(argv[2]);
+// If call to service successful then display sum
 if ( client.call(srv)){
   ROS_INFO_STREAM("Sum: " << (long int)srv.response.sum);
 }
--- beginner_tutorials/src/talker.cpp
+++ beginner_tutorials/src/talker.cpp
@@ -1,29 +1,15 @@
 /*
  * Copyright (C) 2008, Morgan Quigley and Willow Garage, Inc.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *   * Redistributions of source code must retain the above copyright notice,
- *     this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above copyright
- *     notice, this list of conditions and the following disclaimer in the
- *     documentation and/or other materials provided with the distribution.
- *   * Neither the names of Stanford University or Willow Garage, Inc. nor the names of its
- *     contributors may be used to endorse or promote products derived from
- *     this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- */
+ * Copyright 2020 Nidhi Bhojak 
+ * @file talker.cpp 
+ * @author Nidhi Bhojak
+ * 
+ * @brief ROS Service
+ * 
+ * @section DESCRIPTION 
+ * 
+ * Source file for ROS service to add two integers 
+ * */
 // %Tag(FULLTEXT)%
 // %Tag(ROS_HEADER)%
 #include <sstream>
@@ -35,6 +21,12 @@
 #include "beginner_tutorials/AddTwoInts.h"
 
 // Funtion to provide service to add two ints
+/**
+ * @brief Add two integers function
+ * @param req Add two integers request object 
+ * @param res Add two integers response object
+ * @return True on addition success
+ *  **/
 bool add(beginner_tutorials::AddTwoInts::Request &req,
          beginner_tutorials::AddTwoInts::Response &res){
         res.sum = req.a + req.b;
@@ -46,6 +38,12 @@ bool add(beginner_tutorials::AddTwoInts::Request &req,
 /**
  * This tutorial demonstrates simple sending of messages over the ROS system.
  */
+/** 
+ * @brief Main function
+ * @param argc: Command line number of arguments
+ * @param argv: Command line arguments vector
+ * @return 0
+ * **/
 int main(int argc, char **argv) {
   /**
    * The ros::init() function needs to see argc and argv so that it can perform
@@ -58,6 +56,7 @@ int main(int argc, char **argv) {
    * part of the ROS system.
    */
 // %Tag(INIT)%
+// Initialise ROS Node
   ros::init(argc, argv, "talker");
 // %EndTag(INIT)%
 
@@ -67,6 +66,7 @@ int main(int argc, char **argv) {
    * NodeHandle destructed will close down the node.
    */
 // %Tag(NODEHANDLE)%
+// Create ROS node handle object 
   ros::NodeHandle n;
 // %EndTag(NODEHANDLE)%
 
@@ -91,14 +91,16 @@ int main(int argc, char **argv) {
 // %EndTag(PUBLISHER)%
 
 // %Tag(LOOP_RATE)%
+// Loop rate set to 10Hz 
   ros::Rate loop_rate(10);
 // %EndTag(LOOP_RATE)%
 
 ROS_DEBUG_STREAM("Talker started...");
 // Call service
+// Advertise ROS Service 
 ros::ServiceServer service = n.advertiseService("add_two_ints", add);
 ROS_WARN_STREAM("ROS Service might take time to start...");
-ROS_INFO("Adding two ints");
+ROS_INFO_STREAM("Adding two ints");
 ros::spin();
 
 return 0;
