1713a98414cf4cb91e89588585ae3869dae4acf8
nbhojak07 <nidhi.bhojak97@gmail.com>
2020-11-10 16:47:44 -0500

    Added rosbag file
    [beginner_tutorials/src/talker.cpp] Added Publisher message
    
    [beginner_tutorials/src/listener.cpp] Added Subscriber for a published node
    
    [beginner_tutorials/results] Added begineer_tutorials.bag file

--- beginner_tutorials/src/listener.cpp
+++ beginner_tutorials/src/listener.cpp
@@ -1,37 +1,35 @@
 /*
- * Copyright (C) 2008, Morgan Quigley and Willow Garage, Inc.
  * Copyright 2020 Nidhi Bhojak 
- * @file listener.cpp 
+ * @file talker.cpp
  * @author Nidhi Bhojak
- * @date 11/02/2020
+ * @date 11/10/2020
  * 
- * @brief ROS Client 
- * 
- * @section LICENSE
- * MIT License
- * Copyright (c) 2020 Nidhi Bhojak
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- * 
- * @section DESCRIPTION 
- * 
- * Source file for ROS client to add two integers 
- * */
+ * Copyright (C) 2008, Morgan Quigley and Willow Garage, Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *   * Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *   * Neither the names of Stanford University or Willow Garage, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
 // %Tag(FULLTEXT)%
 #include "ros/ros.h"
 #include "std_msgs/String.h"
@@ -40,12 +38,16 @@
 /**
  * This tutorial demonstrates simple receipt of messages over the ROS system.
  */
-/**
- * @brief Main function
- * @param argc Command line number of arguments
- * @param argv Command line arguments vector 
- * @return 0
- *  **/
+
+/** 
+ * @brief Chatter topic callback function
+ * @param msg received 
+ * @return None 
+ * **/
+void chatterCallback( const std_msgs::String::ConstPtr& msg){
+  ROS_INFO("Hey There, it's me [%s]", msg->data.c_str());
+}
+
 int main(int argc, char **argv) {
   /**
    * The ros::init() function needs to see argc and argv so that it can perform
@@ -57,36 +59,22 @@ int main(int argc, char **argv) {
    * You must call one of the versions of ros::init() before using any other
    * part of the ROS system.
    */
-  // Initialise ROS Node
   ros::init(argc, argv, "listener");
-  // Check argument length vector
-  if (argc != 3) {
+
+  if (argc != 3){
     ROS_INFO_STREAM("Usage: add_two_ints_client X Y");
     return 1;
   }
 
-  /**
-   * NodeHandle is the main access point to communications with the ROS system.
-   * The first NodeHandle constructed will fully initialize this node, and the last
-   * NodeHandle destructed will close down the node.
-   */
-  // Create ROS Node handle object
   ros::NodeHandle n;
-
+  ros::Subscriber sub = n.subscribe("chatter", 1000, chatterCallback);
 // ROS Service Client
 
-ros::ServiceClient client = n.serviceClient<
-              beginner_tutorials::AddTwoInts>("add_two_ints");
+ros::ServiceClient client = n.serviceClient<beginner_tutorials::AddTwoInts>("add_two_ints");
 beginner_tutorials::AddTwoInts srv;
 srv.request.a = atoll(argv[1]);
 srv.request.b = atoll(argv[2]);
-// If call to service successful then display sum
-if ( client.call(srv) ) {
-  ROS_INFO_STREAM("Sum: " << (long int)srv.response.sum);
-} else {
-  ROS_ERROR_STREAM("Failed to call service add_two_ints");
-  ROS_FATAL_STREAM("Failed to call service add_two_ints!!");
-  return -1;
-}
+
+ros::spin();
 return 0;
 }
--- beginner_tutorials/src/talker.cpp
+++ beginner_tutorials/src/talker.cpp
@@ -1,5 +1,10 @@
 /*
- * Copyright (C) 2008, Morgan Quigley and Willow Garage, Inc.
+ *  Copyright 2020 Nidhi Bhojak
+ *  @file talker.cpp
+ *  @author Nidhi Bhojak
+ *  @date 11/10/2020
+ * 
+ *  Copyright (C) 2008, Morgan Quigley and Willow Garage, Inc.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -29,10 +34,7 @@
 #include <sstream>
 #include "tf/transform_broadcaster.h"
 #include "ros/ros.h"
-// %EndTag(ROS_HEADER)%
-// %Tag(MSG_HEADER)%
 #include "std_msgs/String.h"
-// %EndTag(MSG_HEADER)%
 #include "beginner_tutorials/AddTwoInts.h"
 
 // Funtion to provide service to add two ints
@@ -45,63 +47,28 @@ bool add(beginner_tutorials::AddTwoInts::Request &req,
         }
 
 /**
- * This tutorial demonstrates simple sending of messages over the ROS system.
+ * @brief Main function
+ * @param argc Commandline number of arguments
+ * @param argv Commandline arguments vector
+ * @return 0
  */
 int main(int argc, char **argv) {
-  /**
-   * The ros::init() function needs to see argc and argv so that it can perform
-   * any ROS arguments and name remapping that were provided at the command line.
-   * For programmatic remappings you can use a different version of init() which takes
-   * remappings directly, but for most command-line programs, passing argc and argv is
-   * the easiest way to do it.  The third argument to init() is the name of the node.
-   *
-   * You must call one of the versions of ros::init() before using any other
-   * part of the ROS system.
-   */
-// %Tag(INIT)%
+  // Initialise ROS node
   ros::init(argc, argv, "talker");
-// %EndTag(INIT)%
 
-  /**
-   * NodeHandle is the main access point to communications with the ROS system.
-   * The first NodeHandle constructed will fully initialize this node, and the last
-   * NodeHandle destructed will close down the node.
-   */
-// %Tag(NODEHANDLE)%
+  // ROS handle object
   ros::NodeHandle n;
-// %EndTag(NODEHANDLE)%
 
-  /**
-   * The advertise() function is how you tell ROS that you want to
-   * publish on a given topic name. This invokes a call to the ROS
-   * master node, which keeps a registry of who is publishing and who
-   * is subscribing. After this advertise() call is made, the master
-   * node will notify anyone who is trying to subscribe to this topic name,
-   * and they will in turn negotiate a peer-to-peer connection with this
-   * node.  advertise() returns a Publisher object which allows you to
-   * publish messages on that topic through a call to publish().  Once
-   * all copies of the returned Publisher object are destroyed, the topic
-   * will be automatically unadvertised.
-   *
-   * The second parameter to advertise() is the size of the message queue
-   * used for publishing messages.  If messages are published more quickly
-   * than we can send them, the number here specifies how many messages to
-   * buffer up before throwing some away.
-   */
-// %Tag(PUBLISHER)%
-// %EndTag(PUBLISHER)%
-
-// %Tag(LOOP_RATE)%
+  // Set frequency to 10Hz
   ros::Rate loop_rate(10);
-// %EndTag(LOOP_RATE)%
 
 ROS_DEBUG_STREAM("Talker started...");
+ros::Publisher chatterPub = n.advertise<std_msgs::String>("chatter", 1000);
+
 // Call service
 ros::ServiceServer service = n.advertiseService("add_two_ints", add);
 ROS_WARN_STREAM("ROS Service might take time to start...");
 ROS_INFO_STREAM("Adding two ints");
-
-ros::spin();
 
 while (ros::ok()) {
   // Transform braodcaster 
@@ -115,8 +82,15 @@ while (ros::ok()) {
                                         ros::Time::now(),
                                         "world",
                                         "talk"));
+  std_msgs::String msg;
+  std::stringstream ss;
+  ss << "HELLO";
+  msg.data = ss.str();
+  chatterPub.publish(msg);
+  ros::spinOnce();
   loop_rate.sleep();
 }
+ros::spin();
 return 0;
 }
 // %EndTag(FULLTEXT)%
